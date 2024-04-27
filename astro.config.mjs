9db33a3c732173a21a1caf9616bed3426398d0e5
Prakhar Shukla <dev@prakhar.codes>
2024-04-27 23:25:09 +0530

    fix: url in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,32 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import remarkToc from "remark-toc";
+import { remarkReadingTime } from "./src/plugins/reading-time.mjs";
+
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://prakharshukla.dev",
+  site: "https://prakhar.codes",
+  markdown: {
+    remarkPlugins: [
+      [
+        //configure to show h2 and h1 as well
+        remarkToc,
+        {
+          headings: ["h1", "h2"],
+          ordered: false,
+          tight: true,
+        },
+      ],
+      remarkReadingTime,
+    ],
+    shikiConfig: {
+      wrap: true,
+      theme: "one-dark-pro",
+    },
+  },
   integrations: [
     mdx(),
     sitemap(),
